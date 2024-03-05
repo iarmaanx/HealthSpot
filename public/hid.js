9ed814b761a0fa0349cd7a0aa0588f660287d82a
iarmaansingh <thearmaandeep@gmail.com>
2024-03-05 19:49:45 +0530

    s

---
+++ public/hid.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const healthIdInput = document.getElementById('health-id');
+
+    // Call the function to generate a random ID when the page loads
+    generateRandomId();
+
+    function generateRandomId() {
+        const min = 100000000; // 9 digits starting from 100,000,000
+        const max = 999999999; // 9 digits ending at 999,999,999
+        const randomId = Math.floor(Math.random() * (max - min + 1)) + min;
+        healthIdInput.value = randomId;
+    }
+});
+
+function openNewPage() {
+     
+      var newPageUrl = "/index.html";
+     
+      window.open(newPageUrl, "_blank");
+    }
