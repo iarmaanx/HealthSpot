944a86ef068040e25872b8cd7aeb4c8a0729949c
iarmaanx <thearmaandeep@gmail.com>
2025-04-25 22:51:18 +0530

    Updated Running

--- public/hid.js
+++ public/hid.js
@@ -1,20 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const healthIdInput = document.getElementById('health-id');
-
+    const qrImage = document.getElementById('qr-code');
+  
     // Call the function to generate a random ID when the page loads
     generateRandomId();
-
+  
     function generateRandomId() {
-        const min = 100000000; // 9 digits starting from 100,000,000
-        const max = 999999999; // 9 digits ending at 999,999,999
+        const min = 100000000;
+        const max = 999999999;
         const randomId = Math.floor(Math.random() * (max - min + 1)) + min;
         healthIdInput.value = randomId;
+      
+        const medicalRecord = `
+      Health ID: ${randomId}
+      Name: Armaan Khan
+      Age: 21
+      Gender: Male
+      Blood Group: B+
+      Allergies: None
+      Medical History: Asthma (2019), Knee Surgery (2022)
+      Emergency Contact: 9876543210
+      `;
+      
+        const qrData = encodeURIComponent(medicalRecord);
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${qrData}&size=150x150`;
+      }
+      
     }
-});
-
-function openNewPage() {
-     
-      var newPageUrl = "/public/main.html";
-     
-      window.open(newPageUrl, "_blank");
-    }
+  );
+  
